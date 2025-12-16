dda03427f6191117fc0e6a286283f843fd188834
abittar-sony <alexandre.bittar@sony.com>
2025-12-16 11:40:25 +0000

    removed unnecessary args, i.e., considered distill_cfg to always be true, use_lora to always be false etc, removed add_extra_key and teacher_ldm

--- sfxfm/model/meanflow_from_pretrained.py
+++ sfxfm/model/meanflow_from_pretrained.py
@@ -2,24 +2,17 @@
 class MeanFlowFromPretrained:
 
     MeanFlowFromPretrained is a wrapper for a pretrained Latent Diffusion Model (LDM)
-    with meanflow-specific modifications, such as custom preprocessing, LoRA support
-    and optional discriminator for adversarial training.
+    with meanflow-specific modifications in preprocessing.
 
 """
 
-import copy
 from pydantic import Discriminator, Tag
-from typing import Annotated, Literal, Union
+from typing import Annotated, Union
 
 import torch
 from torch import nn
 
-from sfxfm.model.dit_blocks import (
-    FourierFeaturesTime,
-    FixedFourierFeaturesTime,
-    ModalityAttentionMFWrapper,
-    ModalityAttention,
-)
+from sfxfm.model.dit_blocks import FourierFeaturesTime, FixedFourierFeaturesTime
 from sfxfm.model.dit_pipeline import DiTFlowMapPipeline
 
 from sfxfm.model.dit_types import DiTArgs, DictTensor
@@ -28,8 +21,6 @@ from sfxfm.model.ldm import (
     LatentDiffusionModelConfig,
     LatentDiffusionModelMeanFlowPipeline,
 )
-
-from sfxfm.model.lora import LoraArgs, shallow_copy
 from sfxfm.components.base import (
     BaseComponent,
     ComponentConfig,
@@ -45,13 +36,6 @@ from sfxfm.components.base import (
 
 class FlowMapPretrainedArgs(ComponentConfig):
     ldm: LatentDiffusionModelConfig
-    pretrained_model_type: Literal["ldm"] = "ldm"
-    use_lora: bool = False
-    lora: LoraArgs
-    pretrain_embeddings: bool = True
-    num_steps_emb_pretraining: int = 20000
-    distill_cfg: bool = False
-    add_discriminator: bool = False
 
 
 FlowMapPretrainedConfig = Annotated[
@@ -61,31 +45,6 @@ FlowMapPretrainedConfig = Annotated[
     ],
     Discriminator(discriminator=_is_load_config),
 ]
-
-
-def add_extra_mod_key_to_modality_attns(
-    module: nn.Module, extra_mod_key: str
-) -> nn.Module:
-    """
-    Returns a shallow copy of module where ModalityAttention layers are replaced with
-    ModalityAttentionMFWrapper layers that include an extra_mod_key for meanflow processing.
-    """
-    new_module = shallow_copy(module)  # shallow copy
-    wrapper_class = ModalityAttentionMFWrapper
-    for name, child in new_module.named_children():
-        if isinstance(child, ModalityAttention):
-            new_child = wrapper_class(
-                attn=child,
-                extra_mod_key=extra_mod_key,
-            )
-        else:
-            new_child = add_extra_mod_key_to_modality_attns(
-                module=child,
-                extra_mod_key=extra_mod_key,
-            )
-        setattr(new_module, name, new_child)
-        # new_module.set_submodule(name, new_child)
-    return new_module
 
 
 class FlipSignPostprocessing(nn.Module):
@@ -129,7 +88,6 @@ class MeanFlowPreprocessing(nn.Module):
         """
         super().__init__()
         self.old_preprocessing = old_preprocessing
-        self.distill_cfg = args.distill_cfg
 
         # Define new timestep embedding modules
         self.timestep_features_t = FixedFourierFeaturesTime(
@@ -139,13 +97,10 @@ class MeanFlowPreprocessing(nn.Module):
             1, dit_args.timestep_features_dim, time_factor=1.0
         )
 
-        if self.distill_cfg:
-            self.cfg_features = FixedFourierFeaturesTime(
-                1, dit_args.timestep_features_dim, time_factor=1.0
-            )
-            cfg_features_dim = dit_args.timestep_features_dim
-        else:
-            cfg_features_dim = 0
+        self.cfg_features = FixedFourierFeaturesTime(
+            1, dit_args.timestep_features_dim, time_factor=1.0
+        )
+        cfg_features_dim = dit_args.timestep_features_dim
 
         self.to_timestep_embed = nn.Sequential(
             nn.Linear(
@@ -177,39 +132,19 @@ class MeanFlowPreprocessing(nn.Module):
         # this is an old preprocessing without r:
         d = self.old_preprocessing(x, t, cond, mask)
 
-        # Merge and discard previous t, logvar and logvar_ctm
-        if self.distill_cfg:
-            d["t"] = self.to_timestep_embed(
-                torch.cat(
-                    [
-                        self.timestep_features_t(t[:, None]),
-                        self.timestep_features_r(r[:, None]),
-                        self.cfg_features(cond["cfg"][:, None]),
-                    ],
-                    dim=-1,
-                )
-            )
-        else:
-            d["t"] = self.to_timestep_embed(
-                torch.cat(
-                    [
-                        self.timestep_features_t(t[:, None]),
-                        self.timestep_features_r(r[:, None]),
-                    ],
-                    dim=-1,
-                )
-            )
-
-        d["logvar"] = self.to_logvar(  # (b,)
+        # Merge and discard previous t and logvar
+        d["t"] = self.to_timestep_embed(
             torch.cat(
                 [
-                    self.timestep_logvar(t[:, None]),
-                    self.timestep_logvar(r[:, None]),
+                    self.timestep_features_t(t[:, None]),
+                    self.timestep_features_r(r[:, None]),
+                    self.cfg_features(cond["cfg"][:, None]),
                 ],
                 dim=-1,
             )
-        )[:, 0]
-        d["logvar_ctm"] = self.to_logvar_ctm(  # (b,)
+        )
+
+        d["logvar"] = self.to_logvar(  # (b,)
             torch.cat(
                 [
                     self.timestep_logvar(t[:, None]),
@@ -247,28 +182,19 @@ class MeanFlowFromPretrained(
     def init_pretrained_model(self):
         """
         Initializes the pretrained model pipeline based on the configuration.
-        Depending on the value of `self.config.pretrained_model_type`, this method will:
-        - Instantiate a LatentDiffusionModel if the type is 'ldm', ensuring the model type is supported.
-        - Instantiate an InpaintingFinetune model if the type is 'inpainting-finetune', resolving its DiT configuration.
-        - Raise a ValueError for unsupported or unknown model types.
         Returns:
             Tuple:
-                - ldm: The instantiated pretrained model (LatentDiffusionModel or InpaintingFinetune).
+                - ldm: The instantiated pretrained model (LatentDiffusionModel).
                 - dit_config: The DiT configuration associated with the model.
         Raises:
             ValueError: If the model type is unsupported or unknown.
         """
-        if self.config.pretrained_model_type == "ldm":
-            ldm = LatentDiffusionModel(self.config.ldm)
-            dit_config: DiTArgs = ldm.config.dit
-            supported_models = ["mmmflux", "mmmssflux"]
-            if ldm.config.dit.model_type not in supported_models:
-                raise ValueError(
-                    f"MeanFlowPretrained only supports {supported_models}, got {ldm.config.dit.model_type}"
-                )
-        else:
+        ldm = LatentDiffusionModel(self.config.ldm)
+        dit_config: DiTArgs = ldm.config.dit
+        supported_models = ["mmmflux", "mmmssflux"]
+        if ldm.config.dit.model_type not in supported_models:
             raise ValueError(
-                f"Unknown pretrained_model_type {self.config.pretrained_model_type}"
+                f"MeanFlowPretrained only supports {supported_models}, got {ldm.config.dit.model_type}"
             )
         return ldm, dit_config
 
@@ -282,11 +208,6 @@ class MeanFlowFromPretrained(
 
         # ==== Step 3: init student's LDM pipeline with adapted pre and postprocessing
         ldm, dit_config = self.init_pretrained_model()
-
-        ############### TO DEBUG
-        # print("REMOVE HEREEEEEEEEEEE")
-        # ldm.dit.layers = ldm.dit.layers[:2]
-        ############################
 
         # Define student preprocessing (adapted for 2nd time step r)
         new_preprocessing = MeanFlowPreprocessing(
@@ -318,18 +239,5 @@ class MeanFlowFromPretrained(
             pred_type=ldm.pred_type,
         )
 
-        # ==== Step 4 : Register subcomponents
-        self.register_subcomponent(
-            "backbone_ldm",
-            subcomponent=ldm,
-        )
-
         # Load state dict from its internal _weights_path
         self.load_from_config()
-
-        # Save frozen copy of pretrained teacher model for distillation targets
-        self.teacher_ldm = copy.deepcopy(ldm)
-        self.teacher_ldm.autoencoder = None
-        self.teacher_ldm.conditioners = ldm.conditioners  # to sample from teacher model
-        self.teacher_ldm.requires_grad_(False)
-        self.teacher_ldm.eval()
